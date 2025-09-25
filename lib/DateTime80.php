09efc5c0e31e724cebde80c070e4181780d20803
bam-bam-bigelow <46587439+bam-bam-bigelow@users.noreply.github.com>
2025-09-25 16:59:01 +0200

    Fix DateTime80 constructor to use 'now' for current timestamp conversion

--- lib/DateTime80.php
+++ lib/DateTime80.php
@@ -124,7 +124,7 @@ class DateTime extends \DateTime implements DateTimeInterface
 		if (!$phpDate)
 			return false;
 		// convert to this class using the timestamp
-		$ourDate = new static(null, $phpDate->getTimezone());
+		$ourDate = new static('now', $phpDate->getTimezone());
 		$ourDate->setTimestamp($phpDate->getTimestamp());
 		return $ourDate;
 	}
